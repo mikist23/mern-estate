da6d0b1163d4e9231b000c4f56227ab88038d277
@Mikist <mikeochango3@gmail.com>
2024-01-22 15:31:01 +0300

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 app.get((req,res)=>{
     res.send('Server started for Mikist Estate')
 })
+
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log("Conneted to MongoDB!")
+}).catch((err)=>{
+    console.log(err)
+})
+
 app.listen(3000, ()=>{
     console.log("Server running on port 3000!")
 })
