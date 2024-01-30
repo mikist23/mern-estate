3079c8f61c01f3cd3087501c00b9856500d762bf
@Mikist <mikeochango3@gmail.com>
2024-01-30 18:54:33 +0300

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -211,7 +211,9 @@ function CreateListing() {
                         value={formData.regularPrice}/>
                    <div className="flex flex-col items-center">
                    <p>Regular price</p>
-                   <span className="text-xs"> ($ / month)</span>
+                   {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                    </div>
                 </div>
                 {formData.offer && 
@@ -221,7 +223,9 @@ function CreateListing() {
                         value={formData.discountPrice}/>
                        <div className="flex flex-col items-center">
                        <p>Discounted price</p>
-                       <span className="text-xs"> ($ / month)</span>
+                       {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                        </div>
                     </div>
                 }
