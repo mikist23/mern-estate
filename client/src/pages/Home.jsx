5fd122f8c43c7cd897855fd8328e7380907a1a87
@Mikist <mikeochango3@gmail.com>
2024-03-09 14:39:48 +0300

    Complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import {Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore from 'swiper'
 import 'swiper/swiper-bundle.css';
 import {Navigation} from 'swiper/modules'
+import ListingItem from "../components/ListingItem";
 
 
 function Home() {
@@ -96,9 +97,61 @@ function Home() {
 
       {/**listing results */}
 
-      <div className="">
-        
+      <div className="max-w-6xl mx-auto p-3 flex flex-colm gap-8 my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className=" text-2xl font-semibold text-slate-600">Recent offers</h2>
+              <Link className="text-sm hover:underline text-blue-800" to={'/search?offer=true'}>Show more offers
+
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {offerListings.map((listing)=> (
+                <ListingItem listing={listing} key={listing._id}/>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-colm gap-8 my-10">
+        {rentListings && rentListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className=" text-2xl font-semibold text-slate-600">Recent places for rent</h2>
+              <Link className="text-sm hover:underline text-blue-800" to={'/search?type=rent'}>Show more places for rent
+
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {rentListings.map((listing)=> (
+                <ListingItem listing={listing} key={listing._id}/>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-colm gap-8 my-10">
+        {saleListings && saleListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className=" text-2xl font-semibold text-slate-600">Recent places for sale</h2>
+              <Link className="text-sm hover:underline text-blue-800" to={'/search?type=sale'}>Show more places for sale
+
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {saleListings.map((listing)=> (
+                <ListingItem listing={listing} key={listing._id}/>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      
 
     </div>
   
